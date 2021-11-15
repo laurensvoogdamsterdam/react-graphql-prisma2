c9d159097c704496d5a0253518144663fc7b080e
laurens <voogdlaurens@gmail.com>
2021-11-15 21:55:38 +0100

    Connect deploy backend and release and deploy of web app, IOS and Android app

--- server/src/context.ts
+++ server/src/context.ts
@@ -6,13 +6,21 @@ const prisma = new PrismaClient();
 
 /** PubSub for pagination and realtime fetching */
 const pubsub = new PubSub();
-/** */
+/**
+ * Context interface
+ */
 export interface Context {
   prisma: PrismaClient;
   request: any; // HTTP request carrying the `Authorization` header
   pubsub: PubSub;
 }
 
+
+/**
+ * Context `constructor`
+ * @param request 
+ * @returns 
+ */
 export function createContext(request: any) {
   return {
     ...request,
--- server/src/env.ts
+++ server/src/env.ts
@@ -1,3 +1,6 @@
+/**
+ * Loading env vars into memory accessible by the main threads
+ */
 import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.resolve(__dirname, ".env") });
